8cdff5f62156b6aec548262a3df9eea26cf8dc39
JosselinSomervilleRoberts <josselin.somerville@polytechnique.edu>
2023-05-09 19:59:06 -0700

    Uncomment logging

--- cs224r/critics/cql_critic.py
+++ cs224r/critics/cql_critic.py
@@ -85,8 +85,8 @@ class CQLCritic(BaseCritic):
         
         ### YOUR CODE HERE ###
         loss, qa_t_values, q_t_values = self.dqn_loss(ob_no, ac_na, next_ob_no, reward_n, terminal_n)
-        qa_t_logsumexp = torch.logsumexp(qa_t_values / self.cql_alpha, dim=1)
-        cql_loss = (self.cql_alpha * qa_t_logsumexp - q_t_values).mean()
+        q_t_logsumexp = torch.logsumexp(qa_t_values / self.cql_alpha, dim=1)
+        cql_loss = (self.cql_alpha * q_t_logsumexp - q_t_values).mean()
         loss += cql_loss
         
         ### YOUR CODE HERE ###
@@ -97,9 +97,9 @@ class CQLCritic(BaseCritic):
         
         info = {'Training Loss': ptu.to_numpy(loss)}
         # TODO: Uncomment these lines after implementing CQL
-        # info['CQL Loss'] = ptu.to_numpy(cql_loss)
-        # info['Data q-values'] = ptu.to_numpy(q_t_values).mean()
-        # info['OOD q-values'] = ptu.to_numpy(q_t_logsumexp).mean()
+        info['CQL Loss'] = ptu.to_numpy(cql_loss)
+        info['Data q-values'] = ptu.to_numpy(q_t_values).mean()
+        info['OOD q-values'] = ptu.to_numpy(q_t_logsumexp).mean()
         
         self.learning_rate_scheduler.step()
 
